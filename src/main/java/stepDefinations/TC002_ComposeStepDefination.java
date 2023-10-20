98447be59cc0601f090aea18023d33950254a506
Arpana <muriki.arpana@gmail.com>
2023-10-20 19:35:04 +0530

    Created New Test i.e Compose

---
+++ src/main/java/stepDefinations/TC002_ComposeStepDefination.java
@@ -0,0 +1,12 @@
+package stepDefinations;
+
+import org.testng.annotations.Test;
+
+public class TC002_ComposeStepDefination {
+
+	@Test
+	public void test()
+	{
+		System.out.println("Create New Email..");
+	}
+}
